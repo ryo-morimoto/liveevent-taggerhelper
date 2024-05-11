7ac1ce10ec331c96de89d399041a2f9d253780cc
morimoto-novasto <morimoto@novasto.co.jp>
2024-05-11 23:49:29 +0900

    refactor: 💡 アプリケーションはsrcディレクトリに置くように変更

--- src/popup.tsx
+++ src/popup.tsx
@@ -6,15 +6,9 @@ function IndexPopup() {
   return (
     <div
       style={{
-        padding: 16
+        padding: 4
       }}>
-      <h2>
-        Welcome to your{" "}
-        <a href="https://www.plasmo.com" target="_blank">
-          Plasmo
-        </a>{" "}
-        Extension!
-      </h2>
+      
       <input onChange={(e) => setData(e.target.value)} value={data} />
       <a href="https://docs.plasmo.com" target="_blank">
         View Docs
